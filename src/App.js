54c577c3db69b410a9d24c0378da639fa71f1d7e
Fahad <fahadmahmood1200@gmail.com>
2021-09-27 23:13:01 +0530

    Added search functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import ImageCard from "./components/ImageCard";
-
+import ImageSearch from "./components/ImageSearch";
 
 function App() {
 
@@ -10,23 +9,28 @@ function App() {
   const [term, setTerm] = useState('');
 
   useEffect (() => {
-  fetch(`https://pixabay.com/api/?key=23585030-cbd9d874703043984c9330ad3&q=yellow+flowers&image_type=photo&pretty=true`)
+  fetch(`https://pixabay.com/api/?key=23585030-cbd9d874703043984c9330ad3&q=${term}&image_type=photo&pretty=true`)
    .then(res => res.json())
    .then(data => {
      setImages(data.hits);
      setIsLoading(false);
    })
    .catch(err => console.log(err));
-  },[]);
+  },[term]);
 
 
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-3 gap-4">
+
+      <ImageSearch searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-2">No Images Found</h1>}
+
+     {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
         {images.map(image => (
         <ImageCard key={image.id} image={image}/> 
         ))}
-      </div>
+      </div>}
     </div>
     );
 }
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 function ImageCard({image}) {
+
+    const tags = image.tags.split(',')
+
     return (
         <div className="max-w-sm rounded overflow-hidden shadow-lg m-5 ">
         <img src={image.webformatURL}  className="h-100 w-full  " />
@@ -24,15 +27,11 @@ function ImageCard({image}) {
          </ul>
         </div>
         <div className="px-6 py-4">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1  text-sm font-semibold text-gray-700 me-2">
-            #tag 1
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 mx-2 text-sm font-semibold text-gray-700 me-2">
-            #tag 2
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 mx-2 text-sm font-semibold text-gray-700 me-2">
-            #tag 3
-          </span>
+         {tags.map((tag, index) => (
+              <span key={index}className="inline-block bg-gray-200 rounded-full px-3 py-1 m-1 text-sm font-semibold text-gray-700 me-2">
+              #{tag}
+            </span>
+         ))}
         </div>
       </div>
     )
